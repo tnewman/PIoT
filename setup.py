87757d7a57a55e6bc7c5e67e085cc3e5aa72bcc0
Thomas Newman <tnewman1@ltu.edu>
2016-01-10 21:50:32 +0000

    Initial Project Skeleton

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='PIoT',
+    description='Internet of Things Platform for Raspberry PI',
+    version='0.1dev',
+    packages=['piot',],
+    license='MIT',
+    setup_requires=[
+        'pytest-runner',
+    ],
+    install_requires=[
+        'python-periphery',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
