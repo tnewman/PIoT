9ae843f02829ed1a8272d5a6a46287641fc573be
Thomas Newman <tnewman1@ltu.edu>
2016-01-14 02:59:52 +0000

    Added Test Logging

--- integrationtests/testsumppumpsensor.py
+++ integrationtests/testsumppumpsensor.py
@@ -4,12 +4,15 @@ sys.path.append("..")
 
 from piot.sensor.sumppump import SumpPump
 
+print('============================')
 print('Sump Pump Sensor Test Script')
-print('Place an object 30cm away from sensor and press enter')
+print('============================')
+print('Place an object 30 cm away from sensor and press enter')
 input()
 
 sump_pump=SumpPump()
 distance=sump_pump.read_analog_sensor()
+print('Object was {0} {1} away'.format(distance, sump_pump.unit))
 
 if(distance>=30-2 or distance<=30+2):
     print('Test Passed')
