fe065d6615a501b225e48b9487bb6928e3697e9c
Thomas Newman <tnewman1@ltu.edu>
2016-01-19 20:56:00 -0500

    Lazy Load Sensors

--- piot/sensor/__init__.py
+++ piot/sensor/__init__.py
@@ -5,19 +5,21 @@ import os
 
 plugin_directory=os.path.dirname(__file__)
 
-# Load all of the modules in this package
-for module in pkgutil.iter_modules([plugin_directory]):
-    name_index = 1
-    importlib.import_module('.' + module[name_index], __package__)
 
+def load_all_package_modules():
+    for module in pkgutil.iter_modules([plugin_directory]):
+        name_index = 1
+        importlib.import_module('.' + module[name_index], __package__)
 
 def get_all_sensor_classes():
     return get_analog_sensor_classes() + \
         get_digital_sensor_classes()
 
 def get_analog_sensor_classes():
+    load_all_package_modules()
     return BaseAnalogSensor.__subclasses__()
 
 
 def get_digital_sensor_classes():
+    load_all_package_modules()
     return BaseDigitalSensor.__subclasses__()
