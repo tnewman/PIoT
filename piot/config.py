122aebbeeb792b3bcd92f7e74aee7ca10790b033
Thomas Newman <tntronic@gmail.com>
2016-02-06 23:50:03 -0500

    Refactored config to return defaults if it cannot read the config.

--- piot/config.py
+++ piot/config.py
@@ -7,16 +7,33 @@ from configparser import ConfigParser
 config = ConfigParser()
 config.read('config.cfg')
 
-twilio_account = config['Twilio']['account']
+def get_with_default(section, name, default):
+    """ Gets a config value or a default if it does not exist.
+    :param section: Section
+    :type section: str
+    :param name: Name
+    :type name: str
+    :param default: Default if it does not exist in the config.
+    :type default: str
+    :return: Value from config or default.
+    :rtype: str
+    """
+
+    if config.has_option(section, name):
+        return config.get(section, name)
+    else:
+        return default
+
+twilio_account = get_with_default('Twilio', 'account', '')
 """ Twilio Account """
 
-twilio_token = config['Twilio']['token']
+twilio_token = get_with_default('Twilio', 'token', '')
 """ Twilio Token """
 
-twilio_sender_number = config['Twilio']['sender_number']
+twilio_sender_number = get_with_default('Twilio', 'sender_number', '')
 """ Number Twilio SMS Notifications will be sent from. """
 
-twilio_recipient_number = config['Twilio']['recipient_number']
+twilio_recipient_number = get_with_default('Twilio', 'recipient_number', '')
 """ Number Twilio SMS Notifications will be sent to. """
 
 sql_alchemy_connection_string = 'sqlite:///piot.db'
